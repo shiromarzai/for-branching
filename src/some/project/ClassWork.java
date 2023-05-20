b1275a8e1d83a13b71fb0ef466ec0b7745d3b7c9
shiromarzai <shahryar_omarzai@hotnmail.com>
2023-05-20 16:31:18 -0700

    from class work

---
+++ src/some/project/ClassWork.java
@@ -0,0 +1,10 @@
+package some.project;
+
+public class ClassWork {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("This Is Frtom Class Work");
+	}
+
+}
